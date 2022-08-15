9165820d7982b1618d971fed04e32db3edfe78f9
Ilya <koselevicila@gmail.com>
2022-08-15 09:40:02 +0300

    commit-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
+import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
