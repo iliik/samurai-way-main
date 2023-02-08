10f1ee511624b627068f20a4f4eefdd6b8e5ee3f
Ilya <koselevicila@gmail.com>
2023-02-08 20:49:08 +0300

    85-useEffect-hook

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,10 +1,14 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 
 export const ProfileStatusWithHooks = (props: any) => {
 
     const [editMode, setEditMode] = useState(false)
     const [status, setStatus] = useState(props.status)
+
+    useEffect(()=>{
+        setStatus(props.status)
+    },[props.status])
 
     const activateMode = () => {
         setEditMode(true)
