eb57beb9fd2c019bb2fda0408b1a87aedcb55f8f
Ilya <koselevicila@gmail.com>
2023-02-14 13:49:37 +0300

    90-TDD-test-refactor-Field

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import style from './FormsControl.module.css'
+import {Field} from "redux-form";
+import {behaviorPlugin} from "@testing-library/user-event/dist/keyboard/types";
 
 
 // @ts-ignore
@@ -25,3 +27,21 @@ export const Input = (props: any) => {
     const {input, meta, child, ...restProps} = props
     return <FormControl {...props} ><input {...input} {...restProps}/></FormControl>
 }
+
+type CreateFieldType = {
+    placeholder: string
+    name: string
+    component: () => void
+    validate: () => void
+    text:string
+    props:{}
+}
+export const createField = (props: CreateFieldType) => (
+    <div><Field placeholder={props.placeholder}
+                name={props.name}
+                component={props.component}
+                validate={props.validate}
+                {...props.props}
+        /> {props.text}</div>
+)
+
--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,38 +1,23 @@
 import React from "react"
 import {Field, reduxForm} from "redux-form";
-import {Input} from "../components/common/FormsControls/FormsControls";
+import {createField, Input} from "../components/common/FormsControls/FormsControls";
 import {required} from "../utils/validator/validators";
 import {connect} from "react-redux";
 import {Navigate} from "react-router-dom";
 import {AppStateType} from "../redux/redux-store";
 import style from './../components/common/FormsControls/FormsControl.module.css'
 
-// type StateType ={
-//     state:InitialStateType
-//     auth:{
-//         isAuth:boolean
-//     }
-// }
-// type formDataType ={
-//     password:null | number
-//     email: null | string,
-//     rememberMe:null | number,
-// }
 
-export const LoginForm = (props: any) => {
-    return <form onSubmit={props.handleSubmit}>
-        <div>
-            <Field placeholder={"Email"} name={'email'} component={Input} validate={[required]}/>
-        </div>
-        <div>
-            <Field placeholder={"Password"} name={'password'} component={Input} type={'password'}
-                   validate={[required]}/>
-        </div>
+export const LoginForm = (handleSubmit: any, error: any) => {
+    return <form onSubmit={handleSubmit}>
+        {createField("Email", "email", [required], Input)}
+        {createField("Password", 'password', [required], Input, {type: 'password'})}
+        {createField(null, "rememberMe", [], Input, {type: 'checkbox'}, "remember me")}
         <div>
             <Field type={'checkbox'} name={'rememberMe'} component={Input}/>remember me
         </div>
-        {props.error && <div className={style.formError}>
-            {props.error}
+        {error && <div className={style.formError}>
+            {error}
         </div>
         }
         <div>
